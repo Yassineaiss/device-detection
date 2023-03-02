fe63730f50b3971064ae35968d57e0f299030e85
Yassine Aissaoui <Yassaiss@gmail.com>
2023-03-02 09:54:14 +0100

    fixing tablet & mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
-
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
-deviceInfo:DeviceInfo | undefined
+  deviceInfo: DeviceInfo | undefined;
 
   title = 'device-detection';
-  constructor(private deviceDetectorService: DeviceDetectorService){}
+  constructor(private deviceDetectorService: DeviceDetectorService) {}
   ngOnInit(): void {
     this.deviceInfo = this.deviceDetectorService.getDeviceInfo();
   }
