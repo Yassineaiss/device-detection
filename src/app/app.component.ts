5ba65dfbf98e7970ff30f9d01555a7d9b420c96c
Yassine Aissaoui <Yassaiss@gmail.com>
2023-03-02 09:45:14 +0100

    Desktop or Mobile Detect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+deviceInfo:DeviceInfo | undefined
+
   title = 'device-detection';
+  constructor(private deviceDetectorService: DeviceDetectorService){}
+  ngOnInit(): void {
+    this.deviceInfo = this.deviceDetectorService.getDeviceInfo();
+  }
 }
